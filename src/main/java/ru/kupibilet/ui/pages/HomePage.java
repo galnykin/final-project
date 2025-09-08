b0ad0ac20c194130664d70d840661e997907279c
Sergey Galnykin <galnykin@gmail.com>
2025-09-08 16:13:14 +0300

    add HomePage

---
+++ src/main/java/ru/kupibilet/ui/pages/HomePage.java
@@ -0,0 +1,18 @@
+package ru.kupibilet.ui.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+
+    private By loginButton = By.id("account-init-button");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
+    }
+}
