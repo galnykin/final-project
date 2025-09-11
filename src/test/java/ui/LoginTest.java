14fcb84c7dacf583160df1d39df177eeb7d8c5d5
Sergey Galnykin <galnykin@gmail.com>
2025-09-11 16:47:11 +0300

    refactor LoginTest.java

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -1,28 +1,21 @@
 package ui;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import ru.kupibilet.ui.drivers.DriverFactory;
 import ru.kupibilet.ui.pages.HomePage;
 import ru.kupibilet.ui.pages.LoginModal;
 import ru.kupibilet.ui.utils.WaitUtils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LoginTest {
-    private WebDriver driver;
+public class LoginTest extends BaseTest {
+
     private HomePage homePage;
     private LoginModal loginModal;
 
     @BeforeEach
-    public void setUp() {
-        driver = DriverFactory.getDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.kupibilet.ru/");
+    public void init() {
         homePage = new HomePage(driver);
         loginModal = new LoginModal(driver);
     }
@@ -30,9 +23,9 @@ public class LoginTest {
     @Test
     public void testLoginFlow() {
         homePage.clickLoginButton();
-        loginModal.enterEmail("myemail@gmail.com");
-        loginModal.enterPassword("fljsolcvm4094u");
-        loginModal.clickSignIn();
+        loginModal.enterEmail("correct.address@gmail.com");
+        loginModal.enterPassword("correctPassword123");
+        loginModal.clickSubmit();
     }
 
     @Test
@@ -40,16 +33,11 @@ public class LoginTest {
         homePage.clickLoginButton();
         loginModal.enterEmail("myemail@gmail.com");
         loginModal.enterPassword("fljsolcvm4094u");
-        loginModal.clickSignIn();
+        loginModal.clickSubmit();
 
         By errorLocator = By.cssSelector("span.sc-qyqger-0.gUhGbX");
         WaitUtils.waitForVisibility(driver, errorLocator);
 
         assertEquals("Вы ошиблись в почте или пароле", loginModal.getErrorMessageText());
-    }
-
-    @AfterEach
-    public void tearDown() {
-        DriverFactory.quitDriver();
     }
 }
