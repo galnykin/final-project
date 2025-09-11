0841b58cb9e5fdeead1da98a7901878657bef98a
Sergey Galnykin <galnykin@gmail.com>
2025-09-11 16:12:28 +0300

    add WaitUtils.java

---
+++ src/main/java/ru/kupibilet/ui/utils/WaitUtils.java
@@ -0,0 +1,19 @@
+package ru.kupibilet.ui.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtils {
+
+    private static final int DEFAULT_TIMEOUT = 10;
+
+    public static WebElement waitForVisibility(WebDriver driver, By errorLocator) {
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(errorLocator));
+    }
+}
