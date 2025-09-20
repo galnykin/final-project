fe082678b1ec3a04256b1690678cf5a65f295c9c
Sergey Galnykin <galnykin@gmail.com>
2025-09-20 20:51:00 +0300

    add HomePageLocators class

---
+++ src/main/java/ru/kupibilet/ui/locators/HomePageLocators.java
@@ -0,0 +1,7 @@
+package ru.kupibilet.ui.locators;
+
+import org.openqa.selenium.By;
+
+public class HomePageLocators {
+    public static final By LOGIN_BUTTON = By.cssSelector("[data-testid='open-auth-modal-button']");
+}
