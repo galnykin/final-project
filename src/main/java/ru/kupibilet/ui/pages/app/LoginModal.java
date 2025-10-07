8b64bfb34720dd4d8f0c4495262fb4f1064a11ac
Sergey Galnykin <galnykin@gmail.com>
2025-10-07 13:11:33 +0300

    refactor code: - change locators to private final from public static final - rename locators - refactor methods code and test to use locators properly

--- src/main/java/ru/kupibilet/ui/pages/app/LoginModal.java
+++ src/main/java/ru/kupibilet/ui/pages/app/LoginModal.java
@@ -4,18 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.kupibilet.ui.pages.base.BasePage;
+import ru.kupibilet.ui.utils.WaitUtils;
 
 public class LoginModal extends BasePage {
 
-    public static final By EMAIL_FIELD = By.cssSelector("[data-testid='email-input']");
-    public static final By PASSWORD_FIELD = By.cssSelector("[data-testid='password-input']");
-    public static final By SIGN_IN_BUTTON = By.cssSelector("[data-testid='sign-in-button']");
-    public static final By REGISTER_LINK = By.linkText("Регистрация");
-    public static final By FORGOT_PASSWORD_LINK = By.linkText("Забыли пароль?");
-    public static final By GENERAL_ERROR_MESSAGE = By.cssSelector("span.sc-qyqger-0.gUhGbX");
-    public static final By EMAIL_ERROR_MESSAGE = By.cssSelector("""
+    private final By emailFieldLocator = By.cssSelector("[data-testid='email-input']");
+    private final By passwordFieldLocator = By.cssSelector("[data-testid='password-input']");
+    private final By signInButtonLocator = By.cssSelector("[data-testid='sign-in-button']");
+    private final By registerLinkLocator = By.linkText("Регистрация");
+    private final By forgotPasswordLinkLocator = By.linkText("Забыли пароль?");
+    private final By authErrorMessageLocator = By.cssSelector("span.sc-qyqger-0.gUhGbX");
+    private final By emailErrorMessageLocator = By.cssSelector("""
             div:has(input[data-testid="email-input"]) + div.styled__StyledContainer-sc-awcqi9-0.iLlVPd""");
-    public static final By PASSWORD_ERROR_MESSAGE = By.cssSelector("""
+    private final By passwordErrorMessageLocator = By.cssSelector("""
             div:has(input[data-testid="password-input"]) + div.styled__StyledContainer-sc-awcqi9-0.iLlVPd""");
 
     public LoginModal(WebDriver driver) {
@@ -24,56 +25,50 @@ public class LoginModal extends BasePage {
 
     public void enterEmail(String email) {
         log.info("Typing email: '{}'", email);
-        type(EMAIL_FIELD, email);
+        type(emailFieldLocator, email);
     }
 
     public void enterPassword(String password) {
         log.info("Typing password");
-        type(PASSWORD_FIELD, password);
+        type(passwordFieldLocator, password);
     }
 
     public void clickSubmit() {
         if (log.isInfoEnabled()) {
-            log.info("Clicking Sign In button [{}]", SIGN_IN_BUTTON);
+            log.info("Clicking Sign In button [{}]", signInButtonLocator);
         }
-        click(SIGN_IN_BUTTON);
+        click(signInButtonLocator);
     }
 
     public void clickRegisterLink() {
-        click(REGISTER_LINK);
+        click(registerLinkLocator);
     }
 
     public void clickForgotPasswordLink() {
-        click(FORGOT_PASSWORD_LINK);
+        click(forgotPasswordLinkLocator);
     }
 
     public WebElement getErrorMessageElement() {
-        return find(GENERAL_ERROR_MESSAGE);
+        return find(authErrorMessageLocator);
     }
 
-    public String getGeneralErrorMessageText() {
-        String message = getText(GENERAL_ERROR_MESSAGE);
-        log.info("General error message: '{}'", message);
+    public String getAuthErrorMessageText() {
+        String message = getText(authErrorMessageLocator);
+        log.info("Waiting for auth error message");
+        WaitUtils.waitForVisibility(driver, authErrorMessageLocator);
+        log.info("Auth error message: '{}'", message);
         return message;
     }
 
     public String getEmailErrorMessageText() {
-        return getText(EMAIL_ERROR_MESSAGE);
+        By message = emailErrorMessageLocator;
+        WaitUtils.waitForVisibility(driver, message);
+        return getText(message);
     }
 
     public String getPasswordErrorMessageText() {
-        return getText(PASSWORD_ERROR_MESSAGE);
-    }
-
-    public By getPasswordErrorMessageLocator() {
-        return PASSWORD_ERROR_MESSAGE;
-    }
-
-    public By getEmailErrorMessageLocator() {
-        return EMAIL_ERROR_MESSAGE;
-    }
-
-    public By getGeneralErrorMessageLocator() {
-        return GENERAL_ERROR_MESSAGE;
+        By message = passwordErrorMessageLocator;
+        WaitUtils.waitForVisibility(driver, message);
+        return getText(message);
     }
 }
