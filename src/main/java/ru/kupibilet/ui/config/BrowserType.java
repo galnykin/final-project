a819692e3f6a8c63b3557bb09fd0c5326d41a360
Sergey Galnykin <galnykin@gmail.com>
2025-10-09 15:24:48 +0300

    add classes: - BrowserType - Config
    refactor code in DriverManager class

---
+++ src/main/java/ru/kupibilet/ui/config/BrowserType.java
@@ -0,0 +1,30 @@
+package ru.kupibilet.ui.config;
+
+/**
+ * Enum representing supported browser types.
+ */
+public enum BrowserType {
+    CHROME,
+    FIREFOX,
+    EDGE;
+
+    /**
+     * Parses a string value into a BrowserType enum.
+     *
+     * @param value browser name as string
+     * @return corresponding BrowserType
+     * @throws IllegalArgumentException if browser is unsupported
+     */
+    public static BrowserType fromString(String value) {
+        switch (value.toLowerCase()) {
+            case "chrome":
+                return CHROME;
+            case "firefox":
+                return FIREFOX;
+            case "edge":
+                return EDGE;
+            default:
+                throw new IllegalArgumentException("Unsupported browser type: " + value);
+        }
+    }
+}
--- src/main/java/ru/kupibilet/ui/drivers/DriverManager.java
+++ src/main/java/ru/kupibilet/ui/drivers/DriverManager.java
@@ -4,48 +4,104 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import ru.kupibilet.ui.config.BrowserType;
+import ru.kupibilet.ui.config.Config;
 
 import java.time.Duration;
 
-    public class DriverManager {
-        private static WebDriver driver;
+/**
+ * Factory class for initializing and managing WebDriver instances.
+ */
+public class DriverManager {
 
-        private DriverManager() {
-            throw new UnsupportedOperationException("Utility class");
+    private static WebDriver driver;
+
+    private DriverManager() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    /**
+     * Returns the singleton WebDriver instance.
+     *
+     * @return WebDriver instance
+     */
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            initDriver();
+        }
+        return driver;
+    }
+
+    /**
+     * Initializes the WebDriver based on the configured browser type.
+     */
+    private static void initDriver() {
+        BrowserType browser = Config.getBrowserType();
+
+        switch (browser) {
+            case CHROME:
+                driver = createChromeDriver();
+                break;
+            case FIREFOX:
+                driver = createFirefoxDriver();
+                break;
+            case EDGE:
+                driver = createEdgeDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported browser type: " + browser);
         }
 
-        public static WebDriver getDriver() {
-            if (driver == null) {
-                initDriver();
+        driver.manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(5))
+                .pageLoadTimeout(Duration.ofSeconds(10));
+    }
+
+    private static WebDriver createChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-notifications");
+        return new ChromeDriver(options);
+    }
+
+    private static WebDriver createFirefoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--width=1920");
+        options.addArguments("--height=1080");
+        return new FirefoxDriver(options);
+    }
+
+    private static WebDriver createEdgeDriver() {
+        WebDriverManager.edgedriver().setup();
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--start-maximized");
+        return new EdgeDriver(options);
+    }
+
+    /**
+     * Quits the WebDriver and releases resources.
+     */
+    public static void quitDriver() {
+        if (driver != null) {
+            try {
+                driver.quit();
+            } finally {
+                driver = null;
             }
-            return driver;
-        }
-
-        private static void initDriver() {
-            WebDriverManager.chromedriver().setup();
-
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--start-maximized");
-            options.addArguments("--disable-notifications");
-
-            driver = new ChromeDriver(options);
-            driver.manage().timeouts()
-                    .implicitlyWait(Duration.ofSeconds(5))
-                    .pageLoadTimeout(Duration.ofSeconds(10));
-        }
-
-        public static void quitDriver() {
-            if (driver != null) {
-                try {
-                    driver.quit();
-                } finally {
-                    driver = null;
-                }
-            }
-        }
-
-        public static void resetDriver() {
-            quitDriver();
-            getDriver();
         }
     }
+
+    /**
+     * Resets the WebDriver by quitting and reinitializing it.
+     */
+    public static void resetDriver() {
+        quitDriver();
+        getDriver();
+    }
+}
